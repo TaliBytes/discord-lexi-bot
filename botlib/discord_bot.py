d7c622a9f6369fecd9e14b1fffdc22e8589bdf33
TaliBytes <shaf.talia1@gmail.com>
2024-11-03 10:49:52 -0700

    discord_bot.py initial test config

--- botlib/discord_bot.py
+++ botlib/discord_bot.py
@@ -1,28 +1,26 @@
 import discord
 
-tokenFile = open('D:/Code/discord-bot/token.txt', 'r')
-print(tokenFile[1].strip())
-
-# #config
-# aToken = ''
-# intents = discord.Intents.default()
-# intents.message_content = True
-
-# client = discord.Client(intents=intents)
+#config
+tokenFile = open('D:/Code/discord-bot/root/token.txt', 'r')
+aToken = tokenFile.readline().strip()
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
 
-# #when bot is logged in
-# @client.event
-# async def on_ready():
-#     print(f'Logged in as {client.user}')
+#when bot is logged in
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
 
-# #when both sees a messages
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-    
-#     if message.content.startswith('$hello'):
-#         await message.channel.send('Hiya!')
 
-# client.run(aToken)
+#when bot sees a messages
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hiya!')
+
+client.run(aToken)
