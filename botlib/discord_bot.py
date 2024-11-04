e81bf16d79028bdb0da2c85acb3f7932f10fa174
TaliBytes <shaf.talia1@gmail.com>
2024-11-03 20:50:43 -0700

    v.0.4 - message has to be a cmd ONLY to be processed as a cmd. parseCmd uppercases cmdName to make it easier to identity start point of a cmd (e.g SAY vs say... SAY is easier to read). Better debugging messages.

--- botlib/discord_bot.py
+++ botlib/discord_bot.py
@@ -1,7 +1,14 @@
 import discord
+import os
+
+cwd = os.getcwd()
+files = os.listdir(cwd)
+print("Files in %r: %s" % (cwd, files))
+
+
 
 #config
-tokenFile = open('D:/Code/discord-bot/root/token.txt', 'r')
+tokenFile = open('root/token.txt', 'r')
 aToken = tokenFile.readline().strip()
 tokenFile.close()
 
@@ -38,11 +45,11 @@ def parseCmd(cmd):
         cmdArgs = None                      #no args were supplied
 
     else:
-        cmdArgs = cmd.split(cmdDelim)           #extract arguments from full command
-        cmdName = cmdArgs[0]                    #extract name from arglist (first argument)
-        cmdArgs = cmdArgs[1:]                   #remove cmdName from args  (remaining arguments)
+        cmdArgs = cmd.split(cmdDelim)       #extract arguments from full command
+        cmdName = cmdArgs[0]                #extract name from arglist (first argument)
+        cmdArgs = cmdArgs[1:]               #remove cmdName from args  (remaining arguments)
 
-    return(cmdName, cmdArgs)
+    return(cmdName.upper(), cmdArgs)
 
 
 
@@ -60,16 +67,17 @@ async def on_message(msg):
         return
     
     #if command
-    if msg.content.startswith('${'):
+    if (msg.content.startswith('${') & msg.content.endswith('}')):
         aCmd = parseCmd(msg.content)
-        cmdName = aCmd[0].lower()
+        cmdName = aCmd[0]
         cmdArgs = aCmd[1]
 
         #used for debug
-        print('Received command ${' + cmdName + '} with args ' + (', ').join(cmdArgs) if cmdArgs else 'not supplied')
+        print('Received command ${' + cmdName.lower() + '} from ' + str(msg.author) + ' with args ' + (', '.join(f'"{arg}"' for arg in cmdArgs) if cmdArgs else 'not supplied'))
 
         #is command valid?
         if cmdName not in cmdList:
+            print('${' + cmdName + '} is an invalid command.')
             await msg.channel.send('${' + cmdName + '} is not a valid command. Do $\{help\} to get a list of commands.')
             return
         else: 
@@ -78,21 +86,34 @@ async def on_message(msg):
 
         #not enough args supplied
         if (len(isnone(cmdArgs,'')) < requiredArgs):
-            cmdSyntax = cmdList[cmdName][1]      #get syntax
+            cmdSyntax = cmdList[cmdName][1]             #get syntax for error message
+            print('Incorrect ${' + cmdName + '} syntax; correct: ' + cmdSyntax)
             await msg.channel.send('${' + cmdName + '} requires ' + str(requiredArgs) + ' argument(s); ' + 'syntax: ' + cmdSyntax)
             return
 
 
-        #ACTUAL COMMAND OPTIONS BEGINS HERE:
-        if (cmdName == 'say'):
+        #ACTUAL COMMAND OPTIONS BEGIN HERE:
+        if (cmdName == 'SAY'):
             await msg.channel.send(cmdArgs[0])
+            return
+        
+        elif (cmdName == 'HELP'):
+            print('HELP command incomplete')
+            return
+
+        else:
+            #the cmd logic is missing or cmd is incorrectly considered valid; so this debug message is sent.
+            print('cmdErr-01... a cmd passed the "cmdName not in cmdList" check, but logic is absent.')
+            await msg.channel.send('cmdErr-01. Please contact a developer to debug.')
 
 
 
 #a list of valid commands, they usage, syntax, and number of arguments that they require
 cmdList = {
     #name, usage, syntax, requiredArgs
-    'say': ['Have Lexi send a message in the current channel.', '${say|message}', 1]
+    'SAY': ['Have Lexi send a message in the current channel.', '${say|message}', 1],
+    'HELP': ['Have Lexi tell list all commands ($\{help\}) or details about one command (${help|cmdName}).', '$\{help\} or ${help|cmdName}', 0],
+    'BADGUY': ['Command intentionally missing logic for debug/test purposes.', '$\{badguy\}', 0]
 }
 
 
